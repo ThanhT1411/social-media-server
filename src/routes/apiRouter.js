b6a58eebfc599df8c640ccf3adc808511a52787d
ThanhT1411 <vuthanh141120@gmail.com>
2023-11-03 20:01:18 +0700

    update

--- src/routes/apiRouter.js
+++ src/routes/apiRouter.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const { register, login } = require('../controllers/userController');
-const { createPost, getAllPosts, addLike, removeLike, createComment } = require('../controllers/postController');
+const { createPost, getAllPosts, addLike, removeLike, createComment, getAllComment, deletePost } = require('../controllers/postController');
 
 const router = express.Router();
 
@@ -8,7 +8,9 @@ router.post('/register', register);
 router.post('/login', login);
 router.post('/post', createPost);
 router.get('/getAllPosts', getAllPosts);
+router.post('/deletePost', deletePost);
 router.post('/comment', createComment)
+router.get('/getAllComment', getAllComment);
 router.post('/addLike', addLike);
 router.post('/removeLike', removeLike);
 
